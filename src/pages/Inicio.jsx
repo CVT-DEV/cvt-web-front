1a6e1ff7b836559ed67e6d8fa61c5680355ee9c9
Giovana Moratelli <giovanamoratelli@gmail.com>
2024-09-17 12:36:49 -0300

    feat (design): home page with 5 buttons

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileLines, faImages, faNewspaper, faRocket, faShuttleSpace, faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons';
 
 
 function Inicio() {
@@ -18,11 +20,49 @@ function Inicio() {
       <div style={styles.footerUp}>
         <Header/>
         <div style={styles.content}>
-          <h3 style={styles.subtitle}>Tela inicial</h3>
-          <div style = {styles.buttonContainer}>
-            <button onClick={navigateGaleria} class="br-sign-in small primary mt-3 mt-sm-0 ml-sm-3" type="button">   
-              Galeria
-            </button>
+          <div style={styles.row}>
+            <div style={styles.toPageDiv}>
+              <div style={styles.toPageText}>
+                <p style={styles.pageName}>Galeria</p>
+              </div>
+              <div style={styles.toPageIcon}>
+                <FontAwesomeIcon icon={faImages} size="3x" color="var(--blue-warm-vivid-70)"/>         
+              </div>
+            </div>
+            <div style={styles.toPageDiv}>
+              <div style={styles.toPageText}>
+                  <p style={styles.pageName}>Materiais Didáticos</p>
+              </div>
+              <div style={styles.toPageIcon}>
+                <FontAwesomeIcon icon={faFileLines} size="3x" color="var(--blue-warm-vivid-70)"/>              
+              </div>
+            </div>
+            <div style={styles.toPageDiv}>
+              <div style={styles.toPageText}>
+                  <p style={styles.pageName}>Notícias</p>
+              </div>
+              <div style={styles.toPageIcon}>
+                  <FontAwesomeIcon icon={faNewspaper} size="3x" color="var(--blue-warm-vivid-70)"/>              
+              </div>
+            </div>
+          </div>
+          <div style={styles.row}>
+            <div style={styles.toPageDiv}>
+              <div style={styles.toPageText}>
+                  <p style={styles.pageName}>Páginas úteis</p>
+              </div>
+              <div style={styles.toPageIcon}>
+                  <FontAwesomeIcon icon={faSquareArrowUpRight} size="3x" color="var(--blue-warm-vivid-70)"/>              
+              </div>
+            </div>
+            <div style={styles.toPageDiv}>
+              <div style={styles.toPageText}>
+                  <p style={styles.pageName}>Dia Espacial</p>
+                </div>
+                <div style={styles.toPageIcon}>
+                  <FontAwesomeIcon icon={faShuttleSpace} size="3x" color="var(--blue-warm-vivid-70)"/>              
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -40,35 +80,52 @@ const styles = {
       position: "relative", 
       minHeight: "100vh"
   },
-  footerUp: {
-      paddingbottom: "48px"
-  },
   content: {
-    position: "absolute",
-    left: 198,
-    top: 166,
+    margin: "auto",
     width: 884,
     height: 548,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingbottom: "48px"
+  },
+  row: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  toPageDiv: {
+    position: "relative",
+    width: 178.73,
+    height: 75.06,
+    margin: "0 50px 45px 0",
+    boxShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.25)"
+  },
+  toPageText: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: 19.06,
     textAlign: "center"
   },
-  subtitle: {
-    fontSize:	"var(--font-size-scale-up-04)",
-    fontWeight:	"var(--font-weight-medium)",
-    lineHeight:	"var(--font-line-height-low)",
-    color: "var(--blue-warm-vivid-70)",
-    marginBottom: "var(--spacing-scale-2xh)",
-    marginTop: "var(--spacing-scale-3xh)"
-  },
-  buttonContainer: {
-    display: "flex", 
-    flexDirection: "column",
+  toPageIcon: {
+    position: "absolute",
+    display: "flex",
+    top: 0,
+    width: "100%",
+    height: 45,
+    justifyContent: "center",
     alignItems: "center"
+  },
+  pageName: {
+    fontSize:	"var(--font-size-scale-up-01)",
+    fontWeight:	"var(--font-weight-regular)",
+    lineHeight:	"var(--font-line-height-low)"
   },
   footerContainer: {
     position: "absolute",
     bottom: 0,
-    width: "100%", 
-    height: "48px"
+    width: "100%"
 },
 }
 
