2df0ffd1b1905c1295d139e8892b244d16be8f77
Giovana Moratelli <giovanamoratelli@gmail.com>
2024-11-29 11:58:57 -0300

    feat (materiais): lists all materials and deletes a material if demanded

--- src/components/Modais/Materiais/ModalDeletarMaterial.jsx
+++ src/components/Modais/Materiais/ModalDeletarMaterial.jsx
@@ -1,10 +1,10 @@
 import "@govbr-ds/core/dist/core.min.css";
 import { api } from "../../../services/api";
 
-export default function ModalDeletarVideo({ onClose, idVideo }) {
+export default function ModalDeletarMaterial({ onClose, idMaterial }) {
 
-    async function deletarVideo(idVideo) {
-        await api.delete(`/videos/${idVideo}`, {
+    async function deletarMaterial(idMaterial) {
+        await api.delete(`/materiais/${idMaterial}`, {
             headers: {
                 'x-access-token': localStorage.getItem('@cvtespacial-web/token'),
             },
@@ -23,17 +23,17 @@ export default function ModalDeletarVideo({ onClose, idVideo }) {
         <div>
             <div class="div br-modal medium" aria-modal="true" role="dialog" aria-labelledby="modalalerttitle">
             <div class="br-modal-header">
-                <div class="modal-title" id="modalalerttitle">Deletar vídeo</div>
+                <div class="modal-title" id="modalalerttitle">Deletar material didático</div>
                 <button class="br-button close circle" type="button" data-dismiss="br-modal" aria-label="Fechar"><i class="fas fa-times" aria-hidden="true"></i>
                 </button>
             </div>
             <div class="br-modal-body">
-                <p>Você tem certeza que deseja deletar esse vídeo?</p>
+                <p>Você tem certeza que deseja deletar esse material didático?</p>
             </div>
             <div class="br-modal-footer justify-content-end">
                 <button class="br-button secondary" type="button" onClick={onClose}>Cancelar
                 </button>
-                <button class="br-button primary ml-2" type="button" onClick={() => {deletarVideo(idVideo)}}>Deletar
+                <button class="br-button primary ml-2" type="button" onClick={() => {deletarMaterial(idMaterial)}}>Deletar
                 </button>
             </div>
             </div>
